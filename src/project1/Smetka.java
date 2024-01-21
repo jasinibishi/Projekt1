796725148b43d2efd2dc5960a45c8c77a7bcbb4c
Jasin Ibishi <jasinibishi@gmail.com>
2024-01-21 14:38:03 +0100

    first commit

---
+++ src/project1/Smetka.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		int jabolki=32;
+		int kompiri=20;
+		int praski=80;
+		double rezultat;
+		rezultat=(jabolki*2)+(kompiri*5)+(praski*2.5);
+		System.out.println("Vkupno vo denari:");
+		System.out.println(rezultat);
+	}
+}
+
